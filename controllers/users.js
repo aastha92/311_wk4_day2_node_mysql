947fe92f68e1abf644fe06862440e6a63dd64174
aastha.92 <npathak@gmail.com>
2019-12-10 23:09:04 -0600

    solution

--- controllers/users.js
+++ controllers/users.js
@@ -12,9 +12,10 @@ const getAllUsers = (req, res) => {
 
 const getUserById = (req, res) => {
   // SELECT USERS WHERE ID = <REQ PARAMS ID>
-  let sql = "QUERY GOES HERE"
+  let sql = "SELECT * FROM ?? WHERE ?? = ?";
+  const replacements = ['users', 'id', (req.params.id)];
   // WHAT GOES IN THE BRACKETS
-  sql = mysql.format(sql, [])
+  sql = mysql.format(sql, replacements)
 
   pool.query(sql, (err, rows) => {
     if (err) return handleSQLError(res, err)
@@ -24,9 +25,14 @@ const getUserById = (req, res) => {
 
 const createUser = (req, res) => {
   // INSERT INTO USERS FIRST AND LAST NAME 
-  let sql = "QUERY GOES HERE"
+  let first_name = req.body.first_name;
+  let last_name = req.body.last_name;
+  let sql = "INSERT INTO users (first_name ,last_name ) VALUES (? , ?)";
+  
+  const replacements = [first_name , last_name];
+  //console.log(req.params.first_name);
   // WHAT GOES IN THE BRACKETS
-  sql = mysql.format(sql, [])
+  sql = mysql.format(sql, replacements)
 
   pool.query(sql, (err, results) => {
     if (err) return handleSQLError(res, err)
@@ -35,10 +41,14 @@ const createUser = (req, res) => {
 }
 
 const updateUserById = (req, res) => {
+  let first_name = req.body.first_name;
+  let last_name = req.body.last_name;
+  let id = req.params.id;
   // UPDATE USERS AND SET FIRST AND LAST NAME WHERE ID = <REQ PARAMS ID>
-  let sql = ""
+  let sql = "UPDATE users SET first_name = ? , last_name = ? WHERE id = ?";
+  const replacements = [first_name,last_name,id];
   // WHAT GOES IN THE BRACKETS
-  sql = mysql.format(sql, [])
+  sql = mysql.format(sql, replacements)
 
   pool.query(sql, (err, results) => {
     if (err) return handleSQLError(res, err)
@@ -48,9 +58,11 @@ const updateUserById = (req, res) => {
 
 const deleteUserByFirstName = (req, res) => {
   // DELETE FROM USERS WHERE FIRST NAME = <REQ PARAMS FIRST_NAME>
-  let sql = ""
+  let first_name = req.params.first_name;
+  let sql = "DELETE FROM users WHERE first_name = ?";
+  const replacements = [first_name];
   // WHAT GOES IN THE BRACKETS
-  sql = mysql.format(sql, [])
+  sql = mysql.format(sql,replacements)
 
   pool.query(sql, (err, results) => {
     if (err) return handleSQLError(res, err)
--- sql/connection.js
+++ sql/connection.js
@@ -1,14 +1,18 @@
 const mysql = require('mysql')
 
+require("dotenv").config(); 
+
+//console.log(process.env.MYSQL_PASSWORD);
+
 class Connection {
   constructor() {
     if (!this.pool) {
       console.log('creating connection...')
       this.pool = mysql.createPool({
         connectionLimit: 100,
-        host: 'localhost',
+        host: '34.70.5.64',
         user: 'root',
-        password: 'password',
+        password: process.env.MYSQL_PASSWORD,
         database: 'admin'
       })
 
